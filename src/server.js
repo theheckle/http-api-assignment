b540a1ff59af58c8a9fca9b3c83bbab7a1e7e5f2
Sam Heckle <samheckle@yahoo.com>
2018-09-08 18:01:27 -0400

    finished setup

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+  console.log(request.url);
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);

0728b2d48c14126f930d2ffad575cf76a4fd9b1c
Sam Heckle <samheckle@yahoo.com>
2018-09-12 14:00:59 -0400

    finished setting up

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 const http = require('http');
+const url = require('url');
+const responseHandler = require('./responses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+const urlStruct = {
+  '/': responseHandler.getIndex,
+};
+
 const onRequest = (request, response) => {
-  console.log(request.url);
+  const parsedUrl = url.parse(request.url);
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response);
+  } else {
+    urlStruct.index(request, response);
+  }
 };
 
 http.createServer(onRequest).listen(port);

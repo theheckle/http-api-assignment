bb05970a7f5530e0c4dabdf102dd83cfd35a6d26
Sam Heckle <samheckle@yahoo.com>
2018-09-17 20:24:53 -0400

    finished

---
+++ src/jsonResponses.js
@@ -0,0 +1,117 @@
+const respond = (request, response, status, object, type) => {
+  response.writeHead(status, {
+    'Content-Type': type,
+  });
+
+  response.write(object);
+  response.end();
+};
+
+const createXML = (responseObj) => {
+  let responseXML = '<response>';
+  if (responseObj.id != null) {
+    responseXML = `${responseXML} <id> ${responseObj.id} </id>`;
+  }
+  responseXML = `${responseXML} <message> ${responseObj.message} </message>`;
+  responseXML = `${responseXML} </response>`;
+  return responseXML;
+};
+
+const checkXMLOrJson = (request, response, responseObj, status, acceptedTypes) => {
+  if (acceptedTypes[0] === 'text/xml') {
+    const responseXML = createXML(responseObj);
+    return respond(request, response, status, responseXML, 'text/xml');
+  }
+
+  const res = JSON.stringify(responseObj);
+  return respond(request, response, status, res, 'application/json');
+};
+
+const success = (request, response, acceptedTypes, params) => {
+  // message to send
+  const responseObj = {
+    message: 'This is a successful response',
+  };
+  return checkXMLOrJson(request, response, responseObj, 200, acceptedTypes);
+};
+
+const badrequest = (request, response, acceptedTypes, params) => {
+
+  if (params.valid === 'true') {
+    const responseObj = {
+      message: 'This request has the required parameters value',
+    };
+    return checkXMLOrJson(request, response, responseObj, 200, acceptedTypes);
+  }
+
+  // message to send
+  const responseObj = {
+    id: 'badRequest',
+    message: 'Missing valid query parameter set to true',
+  };
+
+  return checkXMLOrJson(request, response, responseObj, 400, acceptedTypes);
+};
+
+const unauthorized = (request, response, acceptedTypes, params) => {
+
+  if (params.loggedIn === 'yes') {
+    const responseObj = {
+      message: 'You have successfully viewed the content',
+    };
+    return checkXMLOrJson(request, response, responseObj, 200, acceptedTypes);
+  }
+
+  const responseObj = {
+    id: 'unauthorized',
+    message: 'Missing loggedIn query parameter set to yes',
+  };
+
+  return checkXMLOrJson(request, response, responseObj, 401, acceptedTypes);
+};
+
+const forbidden = (request, response, acceptedTypes, params) => {
+  const responseObj = {
+    id: 'forbidden',
+    message: 'You do not have access to this content',
+  };
+
+  return checkXMLOrJson(request, response, responseObj, 403, acceptedTypes);
+};
+
+const internal = (request, response, acceptedTypes, params) => {
+  const responseObj = {
+    id: 'internalError',
+    message: 'Internal Server Error. Something went wrong',
+  };
+
+  return checkXMLOrJson(request, response, responseObj, 500, acceptedTypes);
+};
+
+const notimplemented = (request, response, acceptedTypes, params) => {
+  const responseObj = {
+    id: 'notImplemented',
+    message: 'A get request has not been implemented yet. Check again later for updated content',
+  };
+
+  return checkXMLOrJson(request, response, responseObj, 501, acceptedTypes);
+};
+
+const notFound = (request, response, acceptedTypes, params) => {
+  const responseObj = {
+    id: 'notImplemented',
+    message: 'The page you are looking for is not found',
+  };
+
+  return checkXMLOrJson(request, response, responseObj, 404, acceptedTypes);
+};
+
+module.exports = {
+  success,
+  badrequest,
+  unauthorized,
+  forbidden,
+  internal,
+  notimplemented,
+  notFound,
+};
--- src/server.js
+++ src/server.js
@@ -1,21 +1,35 @@
 const http = require('http');
 const url = require('url');
+const query = require('querystring');
 const responseHandler = require('./htmlResponses.js');
+const objectHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
   '/': responseHandler.getIndex,
   '/style.css': responseHandler.getStyle,
+  '/success': objectHandler.success,
+  '/badRequest': objectHandler.badrequest,
+  '/unauthorized': objectHandler.unauthorized,
+  '/forbidden': objectHandler.forbidden,
+  '/internal': objectHandler.internal,
+  '/notImplemented': objectHandler.notimplemented,
+  notFound: objectHandler.notFound,
   index: responseHandler.getIndex,
 };
 
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
+
+  const acceptedTypes = request.headers.accept.split(',');
+
+  const params = query.parse(parsedUrl.query);
+
   if (urlStruct[parsedUrl.pathname]) {
-    urlStruct[parsedUrl.pathname](request, response);
+    urlStruct[parsedUrl.pathname](request, response, acceptedTypes, params);
   } else {
-    urlStruct.index(request, response);
+    urlStruct.notFound(request, response, acceptedTypes, params);
   }
 };
 
